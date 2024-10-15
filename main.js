a0cca69d5cc21201a4e44a6a6589e432bf94980c
Mahmoud-Abdelkarim777 <mabdelkarimkhalaf777@gmail.com>
2024-10-15 07:44:29 +0300

    simi final

---
+++ main.js
@@ -0,0 +1,222 @@
+const translations = {
+    ar: {
+        home: "الرئيسية",
+        about: "من نحن",
+        services: "الخدمات",
+        customers: "العملاء",
+        contact: "تواصل معنا",
+        // sidebar
+        "menu-title": "القائمة",
+        "tagline": "نقف بجانبك بخبرةٍ تعزز حقوقك والتزامٍ يحقق لك العدالة",
+        "browse-now": "تصفح موقعنا الآن",
+        "talk-us": "تحدث معنا",
+        whatsapp: "واتساب",
+        //about
+        "about-title": "من نحن",
+        "about-highlight": "نحن",
+        "about-description": "تأسســـت شركــــة 'ماني للمحامــــاة' عام 1441 هـ بهـــدف تقــــديم خدمــــــــات قانونيــــــة متكاملـــــة تواكــــب التطــــــورات القانونيـــــــة المحليــــــة والدولية. على مدى السنــــوات الماضيــــة، استطــــاعت الشركـــة أن تبنـــي سمعـــة قوية كشـــريك موثــــوق للعمــلاء من مختلـــــف القطــــاعـــات، بما في ذلك الشــــركــات الكبيــــرة، الأفراد، والمؤسسات الحكومية... <a href='' data-attr='read-more'> أقرأ المزيد</a>",
+        "read-more": "أقرأ المزيد",
+        "view-profile": "عرض ملف الشركة",
+        // services
+        "services-title": "خدماتنا",
+        "services-highlight": "القانونية",
+        "service-1-title": "التقاضي وتسوية المنازعات",
+        "service-1-description": "تقديم خدمات الترافع أمام المحاكم والهيئات القضائية، وحل النزاعات بين الأطـراف بطـرق قانونية تشمل التحكيم والوساطة.",
+        "service-1-link": "أعرف المزيد عن الخدمة",
+        "service-2-title": "الشركات والاستثمار في المملكة",
+        "service-2-description": "تقديم المشورة القانونية لتأسيس الشركات المحليـة والدوليـة، ودعـم الاستثمـارات داخـل المملكـة بمـا يتـوافـق مـع القـوانيـن واللـوائــح.",
+        "service-2-link": "أعرف المزيد عن الخدمة",
+        "service-3-title": "الاستشارات والدراسات القانونية",
+        "service-3-description": "توفير استشارات قانونية دقيقة، بالإضافة إلى إعــداد الـدراســات القـانـونيــة التـي تـدعـم اتخــاذ قـرارات قانونيـة صحيحـة.",
+        "service-3-link": "أعرف المزيد عن الخدمة",
+        "service-4-title": "حماية حقوق الملكية الفكرية",
+        "service-4-description": "مساعدة العملاء في حماية حقوقهم المتعلقة بـالـمـلكــيــة الفكـــريـة، مـثـل تســجـيــل العــلامــــات التجـارية وبـراءات الاخـتراع.",
+        "service-4-link": "أعرف المزيد عن الخدمة",
+        "service-5-title": "التمثيل القانوني والترافع",
+        "service-5-description": "تمثيل العملاء أمام المحاكـم في مختـلـف القضايا القانونية وتقديم الدفاع القانوني عـن حقوقهـم.",
+        "service-5-link": "أعرف المزيد عن الخدمة",
+        "service-6-title": "العقارات والمقاولات وإدارة الأملاك",
+        "service-6-description": "قديم المشورة القانونية في صفقات العقارات والمقـاولات، بما في ذلك شراء وبيـع العقارات وتنفيـذ عقـود البنـاء والتطـوير.",
+        "service-6-link": "أعرف المزيد عن الخدمة",
+        //swiper
+        "swiper-service-1-title": "التقاضي وتسوية المنازعات",
+        "swiper-service-1-description": "تقديم خدمات الترافع أمام المحاكم والهيئات القضائية، وحل النزاعات بين الأطـراف بطـرق قانونية تشمل التحكيم والوساطة.",
+        "swiper-service-1-link": "أعرف المزيد عن الخدمة",
+        "swiper-service-2-title": "الشركات والاستثمار في المملكة",
+        "swiper-service-2-description": "تقديم المشورة القانونية لتأسيس الشركات المحليـة والدوليـة، ودعـم الاستثمـارات داخـل المملكـة بمـا يتـوافـق مـع القـوانيـن واللـوائــح.",
+        "swiper-service-2-link": "أعرف المزيد عن الخدمة",
+        "swiper-service-3-title": "الاستشارات والدراسات القانونية",
+        "swiper-service-3-description": "توفير استشارات قانونية دقيقة، بالإضافة إلى إعــداد الـدراســات القـانـونيــة التـي تـدعـم اتخــاذ قـرارات قانونيـة صحيحـة.",
+        "swiper-service-3-link": "أعرف المزيد عن الخدمة",
+        "swiper-service-4-title": "حماية حقوق الملكية الفكرية",
+        "swiper-service-4-description": "مساعدة العملاء في حماية حقوقهم المتعلقة بـالـمـلكــيــة الفكـــريـة، مـثـل تســجـيــل العــلامــــات التجـارية وبـراءات الاخـتراع.",
+        "swiper-service-4-link": "أعرف المزيد عن الخدمة",
+        "swiper-service-5-title": "التمثيل القانوني والترافع",
+        "swiper-service-5-description": "تمثيل العملاء أمام المحاكـم في مختـلـف القضايا القانونية وتقديم الدفاع القانوني عـن حقوقهـم.",
+        "swiper-service-5-link": "أعرف المزيد عن الخدمة",
+        "swiper-service-6-title": "العقارات والمقاولات وإدارة الأملاك",
+        "swiper-service-6-description": "قديم المشورة القانونية في صفقات العقارات والمقـاولات، بما في ذلك شراء وبيـع العقارات وتنفيـذ عقـود البنـاء والتطـوير.",
+        "swiper-service-6-link": "أعرف المزيد عن الخدمة",
+        "view-all-services": "عرض جميع الخدمات",
+        "legal-support-message": "احجز الآن لتلقي الدعم القانوني من فريقنا ذي الخبرة الواسعة، وتجنب المخاطر القانونية",
+        "book-consultation": "احجز استشارتك الآن",
+        // Partners of Success Section
+        "partners-success-title": "شركاء <span class='word-color'>النجاح</span>",
+        // Footer Section
+        "quick-links-title": "روابط سريعة",
+        "link-home": "الرئيسية",
+        "link-about": "من نحن",
+        "link-services": "الخدمات",
+        "link-customers": "العملاء",
+        "link-contact": "تواصل معنا",
+        "contact-info-title": "معلومات الاتصال",
+        "location-icon": "رمز الموقع",
+        "location-text": "الـــــرياض - الخبــــــر - جــــــــدة",
+        "phone-icon": "رمز الهاتف",
+        "phone-text": "+966 50 225 2450",
+        "email-icon": "رمز البريد الإلكتروني",
+        "email-text": "manilaw@mani-law.com",
+        "website-icon": "رمز الموقع الإلكتروني",
+        "website-text": "www.mani-law.com",
+        "copyright": "كل الحقوق محفوظة &copy; 2024",
+        
+    },
+    en: {
+        home: "Home",
+        about: "About Us",
+        services: "Services",
+        customers: "Clients",
+        contact: "Contact Us",
+        // sidebar
+        "menu-title": "Menu",
+        "tagline": "We stand by you with expertise that enhances your rights and a commitment that achieves justice.",
+        "browse-now": "Browse Our Website Now",
+        "talk-us": "Talk to Us",
+        whatsapp: "WhatsApp",
+        // about
+        "about-title": "About Us",
+        "about-highlight": "Us",
+        "about-description": "Founded in 1441 AH, 'Mani Law Firm' aims to provide comprehensive legal services that keep pace with local and international legal developments. Over the past years, the firm has built a strong reputation as a trusted partner for clients from various sectors, including large companies, individuals, and government institutions... <a href='' data-attr='read-more'> Read more</a>",
+        "read-more": "Read more",
+        "view-profile": "View Company Profile",
+        // services
+        "services-title": "Our Services",
+        "services-highlight": "Legal",
+        "service-1-title": "Litigation and Dispute Resolution",
+        "service-1-description": "Providing litigation services before courts and judicial authorities, and resolving disputes between parties through legal means including arbitration and mediation.",
+        "service-1-link": "Learn more about the service",
+        "service-2-title": "Companies and Investment in the Kingdom",
+        "service-2-description": "Providing legal advice for establishing local and international companies, and supporting investments within the Kingdom in accordance with laws and regulations.",
+        "service-2-link": "Learn more about the service",
+        "service-3-title": "Legal Consultations and Studies",
+        "service-3-description": "Providing accurate legal consultations, in addition to preparing legal studies that support making correct legal decisions.",
+        "service-3-link": "Learn more about the service",
+        "service-4-title": "Intellectual Property Rights Protection",
+        "service-4-description": "Assisting clients in protecting their rights related to intellectual property, such as registering trademarks and patents.",
+        "service-4-link": "Learn more about the service",
+        "service-5-title": "Legal Representation and Advocacy",
+        "service-5-description": "Representing clients before courts in various legal cases and providing legal defense for their rights.",
+        "service-5-link": "Learn more about the service",
+        "service-6-title": "Real Estate, Contracting, and Property Management",
+        "service-6-description": "Providing legal advice in real estate and contracting transactions, including buying and selling properties and executing construction and development contracts.",
+        "service-6-link": "Learn more about the service",
+        // swiper
+        "swiper-service-1-title": "Litigation and Dispute Resolution",
+        "swiper-service-1-description": "Providing litigation services before courts and judicial authorities, and resolving disputes between parties through legal means including arbitration and mediation.",
+        "swiper-service-1-link": "Learn more about the service",
+        "swiper-service-2-title": "Companies and Investment in the Kingdom",
+        "swiper-service-2-description": "Providing legal advice for establishing local and international companies, and supporting investments within the Kingdom in accordance with laws and regulations.",
+        "swiper-service-2-link": "Learn more about the service",
+        "swiper-service-3-title": "Legal Consultations and Studies",
+        "swiper-service-3-description": "Providing accurate legal consultations, in addition to preparing legal studies that support making correct legal decisions.",
+        "swiper-service-3-link": "Learn more about the service",
+        "swiper-service-4-title": "Intellectual Property Rights Protection",
+        "swiper-service-4-description": "Assisting clients in protecting their rights related to intellectual property, such as registering trademarks and patents.",
+        "swiper-service-4-link": "Learn more about the service",
+        "swiper-service-5-title": "Legal Representation and Advocacy",
+        "swiper-service-5-description": "Representing clients before courts in various legal cases and providing legal defense for their rights.",
+        "swiper-service-5-link": "Learn more about the service",
+        "swiper-service-6-title": "Real Estate, Contracting, and Property Management",
+        "swiper-service-6-description": "Providing legal advice in real estate and contracting transactions, including buying and selling properties and executing construction and development contracts.",
+        "swiper-service-6-link": "Learn more about the service",
+        "view-all-services": "View All Services",
+        "legal-support-message": "Book now to receive legal support from our experienced team and avoid legal risks.",
+        "book-consultation": "Book Your Consultation Now",
+        // Partners of Success Section
+        "partners-success-title": "Partners of <span class='word-color'>Success</span>",
+        // Footer Section
+        "quick-links-title": "Quick Links",
+        "link-home": "Home",
+        "link-about": "About Us",
+        "link-services": "Services",
+        "link-customers": "Clients",
+        "link-contact": "Contact Us",
+        "contact-info-title": "Contact Information",
+        "location-icon": "Location Icon",
+        "location-text": "Riyadh - Khobar - Jeddah",
+        "phone-icon": "Phone Icon",
+        "phone-link": "Phone Link",
+        "phone-text": "+966 50 225 2450",
+        "email-icon": "Email Icon",
+        "email-text": "manilaw@mani-law.com",
+        "website-icon": "Website Icon",
+        "website-text": "www.mani-law.com",
+        "copyright": "All rights reserved &copy; 2024",
+    
+    }
+};
+
+const setLanguage = (language) => {
+    const elements = document.querySelectorAll("[data-attr]");
+    elements.forEach((element) => {
+        const attribute = element.getAttribute("data-attr");
+        if (translations[language][attribute]) {
+            // console.log(`Setting ${attribute} to ${translations[language][attribute]}`); // Debugging line
+            element.innerHTML = translations[language][attribute];
+        }
+    });
+    document.documentElement.setAttribute("dir", language === "ar" ? "rtl" : "ltr");
+};
+
+const savedLanguage = localStorage.getItem("language") || "en";
+setLanguage(savedLanguage);
+
+const languageSelectors = document.querySelectorAll(".btn-en-glish, .btn-ar-abic, .btn-en, .btn-ar");
+// console.log(languageSelectors);
+
+
+
+let btn_en_glish = document.querySelector(".btn-en-glish"); // en
+let btn_ar_abic = document.querySelector(".btn-ar-abic");  // ar
+
+// دالة لتحديث العرض بناءً على اللغة وحجم الشاشة
+function updateDisplay() {
+    // الحصول على اللغة المخزنة في كل مرة يتم فيها استدعاء هذه الدالة
+    const storedLanguage = localStorage.getItem("language");
+    const isSmallScreen = window.matchMedia("(max-width: 992px)").matches; // حجم الشاشة الصغيرة
+
+    if (storedLanguage === "ar") {
+        btn_en_glish.innerHTML = isSmallScreen ? "En" : "English";
+        btn_ar_abic.style.display = "none";
+        btn_en_glish.style.display = "block";
+    } else if (storedLanguage === "en") {
+        btn_ar_abic.innerHTML = isSmallScreen ? "عر" : "العربية";
+        btn_en_glish.style.display = "none";
+        btn_ar_abic.style.display = "block";
+    }
+
+};
+updateDisplay();
+
+languageSelectors.forEach(selector => {
+    selector.addEventListener("click", (event) => {
+        const language = event.target.dataset.attr === "language-ar" ? "ar" : "en";
+        setLanguage(language);
+        localStorage.setItem("language", language);
+
+        updateDisplay(); // تحديث العرض بعد تغيير اللغة
+    });
+});
+
+// تحديث العرض عند تغيير حجم الشاشة
+window.addEventListener("resize", updateDisplay);
